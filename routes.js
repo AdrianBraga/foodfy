10798c3f59b144441712879d045f29d6d8d6c3c9
Adrian Braga <adrian.braga.ro@gmail.com>
2021-05-11 13:23:45 -0400

    Create page, route show-recipe

--- routes.js
+++ routes.js
@@ -13,7 +13,7 @@ routes.get('/recipe/:id', main.recipe);
 // // Routes ADMIN - RECIPES
 routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
 // routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 // routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
 // // Routes ADMIN - USER
--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -4,3 +4,23 @@ const data = require('../../data.json');
 exports.index = function(req, res) {
   return res.render('admin/recipes/index', { recipes: data.recipes });
 }
+
+// SHOW
+exports.show = function(req, res) {
+  const { id } = req.params;
+
+  const foundId = data.recipes.find((recipes) => {
+    return recipes.id == id;
+  });
+
+  if(!foundId) {
+    return res.send('Receita não encontrada!')
+  }
+
+  const recipe = {
+    ...foundId,
+    id: foundId
+  }
+
+  return res.render('admin/recipes/show', { recipe })
+}
